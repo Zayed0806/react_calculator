76529f09445bd530e5aaba3623e6429499c7154e
Zayed0806 <132072485+Zayed0806@users.noreply.github.com>
2026-06-06 02:30:50 +0530

    Added Env

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 const dns = require("dns");
 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
-mongoose.connect("mongodb+srv://test_user:test123@cluster0.i6m0ln2.mongodb.net/?appName=Cluster0")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log("MongoDB Connected");
 })
